fdd6d66be4baaf1e00212e9e3cd67012d255d2d2
Florent GALLOU <florentgallou@gmail.com>
2021-03-05 10:14:34 +0100

    strucurized JS created globol vars

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ Welcome to another game project in JavaScript. The purpose is to create a little
 > 1. 04/03/2021 - 12-50 : First commit and push, creation of the files,
 > 2. 04/03/2021 - 12h55 : Create README.md and initialize,
 > 3. 04/03/2021 - 17h22 : Preparing HTML file,
+> 4. 05/03/2021 - 10h12 : Project study check - creat JS structure file + global vars,
---
+++ js/main.js
@@ -0,0 +1,10 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~ GLOBAL VARS ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+var mots = ["furnish", "happyness", "grutt"]; // Words wich the cmp will have the choce to take.
+var chances = 7; // Nb of chances the player can have.
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~ FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~ CODE ~~~~~~~~~~~~~~~~~~~~~~~~~
