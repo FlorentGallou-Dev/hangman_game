5281f57b74d6d852a30029257cb2dc832c565f9e
Florent GALLOU <florentgallou@gmail.com>
2021-03-05 13:27:56 +0100

    add ask lettre & verifi from alphabt

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var words = ["furnish", "happyness", "grutt"]; // Words wich the cmp will have t
 var chances = 7; // Nb of chances the player can have.
 var versus = {
     originalWord : "",
-    transformedWord : ""
+    transformedWord : []
 };
 var letters = []; // contiendra toutes les lettres de l'aphabet
 
@@ -18,29 +18,41 @@ function cmpChoseWord(){
 
 // transform word chosed to _ characters
 function magicWord(word){
-    let result = "";
     for(var i = 0; i < word.length; i++){
-        result += "_";
+        versus.transformedWord[i] = "_";
     }
-    return result;
 }
 
-// creation d'une liste de lettres alphabet
+// fill the tab with all letters of alphabetic
 function alphabet() {
     let letterSet = "abcdefghijklmnopqrstuvwxyz";
     for(var i = 0; i < 26; i++){
         letters[i] = letterSet[i];
     }
-    return letters;
 }
 
-// demande saisie une lettre au jouer + verifications => prévoir supprimer lettres déjà choisies
+// ask to play + verif it's an alphabetic letter or ask again
+function askVerif(){
+    let userLetter = false;
+    do {
+        userLetter = prompt(`Time to play, you still have ${chances} chances to go !!!\nIt's up to you to give the first letter of you choice :`).toLocaleLowerCase();
+        for(var lettre of letters){
+            if(userLetter === lettre) {
+                letters.splice(letters.indexOf(lettre), 1); // Suppr the lettre from the alphabet array
+                return userLetter;
+            }
+        }
+        userLetter = false;
+    }while(!userLetter);
+}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~ CODE ~~~~~~~~~~~~~~~~~~~~~~~~~
 
 alert("Welcome to our brand new game\n                                    THE HANGMAN GAME\n\nPlease enter your fate and see how will it end, mouhahahahahaha !!!");
 versus.originalWord = cmpChoseWord();
-versus.transformedWord = magicWord(versus.originalWord);
+magicWord(versus.originalWord);
 alert(versus.originalWord + " : " + versus.transformedWord);
-alphabet();
+alphabet(); // initialise the alphabet tab in letters
+alert(letters);
+alert(askVerif());
 alert(letters);
