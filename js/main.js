f99024d7015d1ae29c959d89ad9ebf1c35c63698
Florent GALLOU <florentgallou@gmail.com>
2021-03-05 10:32:36 +0100

    cmp chose random word

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~ GLOBAL VARS ~~~~~~~~~~~~~~~~~~~~~~~~~
 
-var mots = ["furnish", "happyness", "grutt"]; // Words wich the cmp will have the choce to take.
+var words = ["furnish", "happyness", "grutt"]; // Words wich the cmp will have the choce to take.
 var chances = 7; // Nb of chances the player can have.
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~ FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
+function cmpChoseWord(){
+    let randomIndex = Math.floor(Math.random() * words.length);
+    return words[randomIndex];
+}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~ CODE ~~~~~~~~~~~~~~~~~~~~~~~~~
 
 alert("Welcome to our brand new game\n                                    THE HANGMAN GAME\n\nPlease enter your fate and see how will it end, mouhahahahahaha !!!");
+alert(cmpChoseWord());
